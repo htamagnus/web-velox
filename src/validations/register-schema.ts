646b76b937ae7cbff9e68cdb6accb053be91a8db
Ágatha <agathamagnusg@hotmail.com>
2025-03-30 08:19:54 -0300

    create register schema

---
+++ src/validations/register-schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+import {
+  strongPasswordRegex,
+  passwordValidationMessage,
+} from '@/helpers/validation'
+
+export const registerSchema = z.object({
+  email: z.string().email('E-mail inválido'),
+  password: z
+    .string()
+    .min(8, 'A senha deve ter no mínimo 8 caracteres.')
+    .regex(strongPasswordRegex, passwordValidationMessage),
+})
