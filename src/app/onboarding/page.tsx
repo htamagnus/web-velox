c674ef93c6996a3a19ebd1602d914830866c73af
Ágatha <agathamagnusg@hotmail.com>
2025-03-30 12:42:22 -0300

    add onboarding page

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+        <h1>onboarding</h1>
+    </main>
+  )
+}
