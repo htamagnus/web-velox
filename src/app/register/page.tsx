e0d4d9d6199ec22367d995b3f3c2c3ac61a0303e
Ágatha <agathamagnusg@hotmail.com>
2025-03-30 09:06:22 -0300

    create a register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/register-form/register-form.component";
+
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+      <RegisterForm />
+    </main>
+  )
+}
