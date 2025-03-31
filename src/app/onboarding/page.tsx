9f11ce4da055bf36dec658f025666c13505229dd
Ágatha <agathamagnusg@hotmail.com>
2025-03-31 18:12:50 -0300

    create page for onboarding

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,7 +1,9 @@
+import OnboardingForm from "@/components/onboarding/onboarding";
+
 export default function RegisterPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
-        <h1>onboarding</h1>
+        <OnboardingForm />
     </main>
   )
 }
