7e942f6ae26938768523bfb1df5beb7ff14afaba
Ágatha <agathamagnusg@hotmail.com>
2025-04-02 08:42:57 -0300

    create normalstep and speedstep

--- src/components/onboarding/steps/index.ts
+++ src/components/onboarding/steps/index.ts
@@ -1,30 +1,51 @@
+import { CreateAthleteDto } from "@/interfaces/athlete.interface"
+import StepHeight from "./step-height"
+import StepWeight from "./step-weight"
+import StepAge from "./step-age"
+import StepSpeed from "./step-speed"
 
-'use client'
-
-import { CreateAthleteDto } from '@/interfaces/athlete.interface'
-import StepWeight from './step-weight'
-import StepHeight from './step-height'
-
-interface StepConfig {
-  key: string
-  Component: React.FC<{
-    value: number
-    onChange: (value: number) => void
-    onNext: () => void
-    onBack?: () => void
-  }>
-  prop: keyof CreateAthleteDto
-}
-
-export const onboardingSteps: StepConfig[] = [
-  {
-    key: 'height',
-    Component: StepHeight,
-    prop: 'height'
-  },
-  {
-    key: 'weight',
-    Component: StepWeight,
-    prop: 'weight'
+type NormalStep = {
+    key: string
+    prop: keyof CreateAthleteDto
+    Component: React.FC<{
+      value: number
+      onChange: (value: number) => void
+      onNext: () => void
+      onBack?: () => void
+    }>
   }
-]
+  
+  type SpeedStep = {
+    key: string
+    props: ['averageSpeedGeneral', 'averageSpeedRoad', 'averageSpeedMtb']
+    Component: React.FC<{
+      values: Pick<CreateAthleteDto, 'averageSpeedGeneral' | 'averageSpeedRoad' | 'averageSpeedMtb'>
+      onChange: (key: keyof CreateAthleteDto, value: number) => void
+      onNext: () => void
+      onBack?: () => void
+    }>
+  }
+  
+  export const onboardingSteps: (NormalStep | SpeedStep)[] = [
+    {
+      key: 'height',
+      prop: 'height',
+      Component: StepHeight
+    },
+    {
+      key: 'weight',
+      prop: 'weight',
+      Component: StepWeight
+    },
+    {
+      key: 'age',
+      prop: 'age',
+      Component: StepAge
+    },
+    {
+      key: 'speed',
+      props: ['averageSpeedGeneral', 'averageSpeedRoad', 'averageSpeedMtb'],
+      Component: StepSpeed
+    }
+  ]
+  
