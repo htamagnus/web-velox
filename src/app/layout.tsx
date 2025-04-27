5fc5e905650612cd2c913ccff6b25259628a58f6
Ágatha <agathamagnusg@hotmail.com>
2025-04-27 15:31:48 -0300

    add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import 'leaflet/dist/leaflet.css'
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <AuthProvider>
           {children}
         </AuthProvider>
+        <Toaster richColors /> 
       </body>
     </html>
   )
