e9cf4736cdd19ce0f3aa49a0e880a459e6d7835f
Ágatha <agathamagnusg@hotmail.com>
2025-04-27 10:29:22 -0300

    update register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,8 +2,9 @@ import RegisterForm from "@/components/register-form/register-form.component";
 
 export default function RegisterPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+    <main className="min-h-screen flex items-center justify-center bg-background text-foreground px-4">
       <RegisterForm />
     </main>
   )
 }
+
