98fb58b9ac3badba6eb543edc20029edd969126a
Ágatha <agathamagnusg@hotmail.com>
2025-04-27 10:29:14 -0300

    update style onboardin page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -2,8 +2,8 @@ import OnboardingForm from "@/components/onboarding/onboarding";
 
 export default function RegisterPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50">
-        <OnboardingForm />
+    <main className="min-h-screen flex items-center justify-center bg-background text-foreground">
+      <OnboardingForm />
     </main>
   )
 }
